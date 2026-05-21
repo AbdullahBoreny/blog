d34fe18a86515e31d1ffe6a47a678ae7b9df9c89
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-05-22 00:54:10 +0300

    add searching mechanism

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
 import { getBlogs } from "../services/blogs";
 
-const Blogs = () => {
-  const blogs = getBlogs();
+const Blogs = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string, }>;
+}) => {
+  const { filter } = await searchParams;
+  const allBlogs = getBlogs();
+  const blogs = filter ? allBlogs.filter(blog => blog.title.includes(filter)) :
+    allBlogs.sort((a, b) => a.likes - b.likes).reverse();
+
   return (
     <div>
+
       <h2>Blogs</h2>
+      <form action="/blogs">
+        <input name="filter" type="text" />
+        <button type="submit">search</button>
+      </form>
       <ul>
         {blogs.map(blog => (
           <ul style={{ marginBottom: 10 }} key={blog.id}>
