76a92d7b6ab4d39d9f8cb13644bfb5302cabf606
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-05-24 02:26:09 +0300

    add error validation

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -4,16 +4,48 @@ import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 import { addBlog, increaseLikes } from "../services/blogs";
 import { auth } from "@/auth";
+interface Error {
+  author: string,
+  title: string;
+  url: string,
 
-export const createBlog = async (formData: FormData) => {
+}
+export const createBlog = async (
+  prevState: { errors: Error, values: Error; },
+  formData: FormData,
+) => {
   const session = await auth();
   if (!session) {
     redirect("/login");
   }
+  const errors = {
+    author: "",
+    url: "",
+    title: ""
+  };
+
   const author = formData.get("author") as string;
+  if (author.length < 3) {
+    const error = "author should be above 5 chars";
+    Object.assign(errors, { author: error });
+  }
   const title = formData.get("title") as string;
-  const likes = formData.get("likes") as string;
+  if (title.length < 3) {
+    const error = "title should be above 5 chars";
+    Object.assign(errors, { title: error });
+
+  }
   const url = formData.get("url") as string;
+  if (url.length < 5) {
+    const error = "url should be above 5 chars";
+    Object.assign(errors, { url: error });
+
+
+  }
+  if (Object.keys(errors).length > 0) {
+    return { errors, values: { title, author, url } };
+  }
+  const likes = formData.get("likes") as string;
 
   await addBlog(author, title, Number(likes), url);
 
--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,35 +1,56 @@
+"use client";
+
 import { createBlog } from "@/app/actions/blogs";
+import { useActionState } from "react";
 
 const NewBlog = () => {
+  const [state, formAction] = useActionState(createBlog, {
+    errors: { author: "", title: "", url: "" },
+    values: { author: "", title: "", url: "" },
+  });
   return (
     <div>
       <h2>Create a new blog</h2>
-      <form action={createBlog}>
+      <form action={formAction}>
         <div>
           <label>
-            Author: 
-            <input type="text" name="author" required />
+            Author:
+            <input
+              type="text"
+              name="author"
+              defaultValue={state.values?.author}
+              minLength={3} />
           </label>
         </div>
         <div>
           <label>
-                Title: 
-            <input type="text" name="title" />
+            Title:
+            <input
+              defaultValue={state.values?.title}
+
+              type="text" name="title" minLength={3} />
           </label>
         </div>
         <div>
           <label>
-                Likes: 
+            Likes:
             <input type="number" name="likes" />
           </label>
         </div>
         <div>
           <label>
-                URL: 
-            <input type="text" name="url" />
+            URL:
+            <input
+              defaultValue={state.values?.url}
+
+              type="text" name="url" minLength={5} />
           </label>
         </div>
         <button type="submit">Create</button>
+        {state.errors.author && <p style={{ color: "red" }}>{state.errors.author}</p>}
+        {state.errors.title && <p style={{ color: "red" }}>{state.errors.title}</p>}
+        {state.errors.url && <p style={{ color: "red" }}>{state.errors.url}</p>}
+
       </form>
     </div>
   );
