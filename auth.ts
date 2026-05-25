ae030046f793bff1afa04f8963e10a7e45c92241
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-05-25 18:37:43 +0300

    edit next auth

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { eq } from "drizzle-orm"
-import bcrypt from "bcryptjs"
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcryptjs";
 import { db } from "./db";
 import { users } from "./db/schema";
 
@@ -14,38 +14,39 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       },
       async authorize(credentials) {
         if (!credentials?.userName || !credentials?.password) {
-          return null
+          return null;
         }
 
         const user = await db.query.users.findFirst({
           where: eq(users.userName, credentials.userName as string),
-        })
+        });
 
         if (!user || !user.passwordHash) {
-          return null
+          return null;
         }
 
         const isValid = await bcrypt.compare(
           credentials.password as string,
           user.passwordHash,
-        )
+        );
 
         if (!isValid) {
-          return null
+          return null;
         }
 
         return {
           id: String(user.id),
           name: user.name,
           email: user.userName,
-        }
+        };
       },
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
   pages: {
     signIn: "/login",
   },
   session: {
     strategy: "jwt",
   },
-})
+});
