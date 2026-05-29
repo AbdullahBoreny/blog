a6c3c1c864ac23454ed8f1d0ad66070b0fac5191
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-05-30 00:00:34 +0300

    remove form

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,7 @@ import "./globals.css";
 const Home = () => {
   return (
     <div className="markdown p-3">
-      <form action="https://send.pageclip.co/xx5I3Vjn4Ld5iqgFmvjDxAAuoSmNuzhG" className="pageclip-form" method="post">
-        <input type="text" name="name" defaultValue="Roscoe Jones" />
-        <input type="email" name="email" defaultValue="roscoe@example.com" />
 
-        <button type="submit" className="pageclip-form__submit">
-          <span>Send</span>
-        </button>
-      </form>
       <Homepage />
 
     </div>
